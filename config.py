93a57dd929f2ed2dc90c60f0ce226bedbe7e7f94
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2022-10-05 15:30:24 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+API_KEY=""
+time_zone="America/Bogota"
+# These settings would be for starting on Feb 8th, 2022 at 11:04:31 (24hr format)
+start_year=2022
+start_month=2
+start_day=8
+start_hour=11
+start_minute=4
+start_second=31
+# Lapse settings
+lapse_interval=10
+lapse_length=120
+lapse_file_name="timelapse.gif"
