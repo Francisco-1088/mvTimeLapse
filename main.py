a6b9b64d4bfd968c180ad17f9a0e3752f3a308c1
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2022-10-05 15:29:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import requests
 import time
 import shutil
 import datetime
+import config
 from dateutil import tz
 
-dashboard = meraki.DashboardAPI(api_key='API_KEY')
-zone = tz.gettz('America/Bogota')
+dashboard = meraki.DashboardAPI(api_key=config.API_KEY)
+zone = tz.gettz(config.time_zone)
 # Set starting date and time
-date = datetime.datetime(2022,2,8,11,4,31, tzinfo=zone)
+date = datetime.datetime(config.start_year,config.start_month,config.start_day,config.start_hour,config.start_minute,config.start_second, tzinfo=zone)
 # Set timelapse interval in seconds
-lapse_int = 120
+lapse_int = config.lapse_interval
 # Set length of timelapse in seconds
-lapse_length = 3600
+lapse_length = config.lapse_length
 
 for i in range(0, lapse_length, lapse_int):
     print(date)
@@ -28,4 +29,4 @@ for i in range(0, lapse_length, lapse_int):
 
 img_string = date.isoformat().split('T')[0]
 
-os.system(f'convert -delay 10 -loop 0 ./lapse/{img_string}*.jpg timelapse.gif')
+os.system(f'convert -delay 10 -loop 0 ./lapse/{img_string}*.jpg {config.lapse_file_name}')
